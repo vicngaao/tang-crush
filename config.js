f11bc56f306e3b5ebe89a906433e481b3aa7b20e
vicngaao <86762540+vicngaao@users.noreply.github.com>
2021-07-01 12:27:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,12 +8,12 @@ const CONFIG = {
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích chị <33',
-    btnNo: 'Không, Anh chị iu à :3',
+    btnNo: 'Không, Chị iu à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu chị <3',
     btnReply: 'Gửi cho chị <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Tui biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h hẹn em chỗ cũ nhe công chúa.',
+    messDesc: 'Tối nay 7h hẹn em chỗ cũ nhe công túa.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
